d138f97eea18cb0d32c2a11c9adb4fe9729d7b34
Brynner03 <bventura03@hotmail.com>
2023-12-29 22:59:52 -0500

    Implemented the API. We're no longer hard-coding data.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
 
   const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
@@ -8,15 +10,26 @@ import { CartService } from 'src/app/services/cart.service';
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   cols = 3;
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
+  products: Array<Product> | undefined;
+  sort = 'desc';
+  count = '12';
+  productSubscription: Subscription | undefined;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private storeService: StoreService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productSubscription = this.storeService.getAllProducts(this.count, this.sort).subscribe((_products) => {
+      this.products = _products;
+    })
   }
 
   onColumnsCountChange(colsNum: number): void {
@@ -36,6 +49,12 @@ export class HomeComponent implements OnInit {
       quantity: 1,
       id: product.id
     });
+  }
+
+  ngOnDestroy(): void {
+      if (this.productSubscription) {
+        this.productSubscription.unsubscribe();
+      }
   }
 
 }
