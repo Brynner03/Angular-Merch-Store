5087a645c2b8dd1ba1d42991a2881e6410acdc12
Brynner03 <bventura03@hotmail.com>
2023-12-31 15:37:30 -0500

    Successfully implemented checkout with stripe.

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -33,7 +35,7 @@ export class CartComponent implements OnInit {
     'action'
   ]
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.cartService.cart.subscribe((_cart: Cart) => {
@@ -60,6 +62,17 @@ export class CartComponent implements OnInit {
 
   onReduceQuantity(item: CartItem): void {
     this.cartService.reduceQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any) => {
+      let stripe = await loadStripe('pk_test_51OSoflCHoohSjYT3XexNtDCTk4vImHI2WgQFyGHes3VMkBWbdu24WSjm349fA2QI7vcISm7oaai1kfk41jiIJMTN0016rO0tGF');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    })
   }
 
 }
