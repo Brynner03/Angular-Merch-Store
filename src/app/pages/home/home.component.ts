7a3d062ccd649b63255d24dfb8838b46d91eef01
Brynner03 <bventura03@hotmail.com>
2023-12-31 14:58:59 -0500

    Dynamically filter products through the categoryies button.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   getProducts(): void {
-    this.productSubscription = this.storeService.getAllProducts(this.count, this.sort).subscribe((_products) => {
+    this.productSubscription = this.storeService.getAllProducts(this.count, this.sort, this.category).subscribe((_products) => {
       this.products = _products;
     })
   }
@@ -39,6 +39,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+    this.getProducts();
   }
 
   onAddToCart(product: Product): void {
